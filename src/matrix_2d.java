4e8ca8b2d233f1f932d95c80b017999182b466aa
SahilSingh8400 <sanju98099@gmail.com>
2026-02-11 09:34:38 +0530

    matrix

---
+++ src/matrix_2d.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class matrix_2d {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        int a[][] = new int[2][3];
+        System.out.println("enter thee elements of array");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(a[i][j] + "\t1");
+            }
+            System.out.println();
+        }
+    }
+}
