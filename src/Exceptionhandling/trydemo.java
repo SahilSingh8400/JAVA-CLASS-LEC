d08c65c859c124fc49854b72eb686cbb714aa259
SahilSingh8400 <sanju98099@gmail.com>
2026-05-14 02:07:49 +0530

    made a exceptionhandlinng folder

---
+++ src/Exceptionhandling/trydemo.java
@@ -0,0 +1,12 @@
+package Exceptionhandling;
+
+public class trydemo {
+    public static void main(String[] args) {
+        int a=0;
+        try{
+            int result=a/0;
+        }catch(ArithmeticException e){
+            System.out.println("case is handled");
+        }
+    }
+}
